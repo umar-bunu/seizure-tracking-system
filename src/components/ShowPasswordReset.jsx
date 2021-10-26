7c00c37f68f26967466ee0e48a35ada530137f8e
umar-bunu <umar.bunu73@gmail.com>
2021-10-26 17:44:40 +0300

    finished by umar bunu

---
+++ src/components/ShowPasswordReset.jsx
@@ -0,0 +1,39 @@
+import React, { useRef } from "react";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import "../styles/styles.css";
+function ShowPasswordReset({ setshouldShowPasswordReset }) {
+  const userEmail = useRef("");
+  return (
+    <div className="showPassword__mainDiv">
+      <div className="showPasswordHeaderDiv">
+        <button
+          onClick={() => {
+            setshouldShowPasswordReset(null);
+          }}
+          className="showPassword__h1Tag"
+        >
+          X
+        </button>
+      </div>
+      <input type="text" ref={userEmail} />
+      <button
+        className="showPassword__submitBtn"
+        onClick={async () => {
+          if (userEmail.current.value != "") {
+            try {
+              await sendPasswordResetEmail(getAuth(), userEmail.current.value);
+              alert("success. Check your email for further instructions");
+              setshouldShowPasswordReset(null);
+            } catch (e) {
+              alert("Something went wrong. Error!");
+            }
+          }
+        }}
+      >
+        Get Password Reset Link
+      </button>
+    </div>
+  );
+}
+
+export default ShowPasswordReset;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useHistory } from "react-router-dom";
 
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import "../styles/styles.css";
+import ShowPasswordReset from "../components/ShowPasswordReset";
 function Login() {
   const [user, loading, error] = useAuthState(getAuth());
   const history = useHistory();
+  const [shouldShowPasswordReset, setshouldShowPasswordReset] = useState(true);
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
@@ -23,6 +25,11 @@ function Login() {
   };
   return (
     <div className="mainDiv">
+      {shouldShowPasswordReset && (
+        <ShowPasswordReset
+          setshouldShowPasswordReset={setshouldShowPasswordReset}
+        />
+      )}
       <h1>Welcome back</h1>
       <h2>Please sign in to continue</h2>
       <form onSubmit={handleSubmit} className="loginForm">
@@ -32,6 +39,14 @@ function Login() {
         <input required type="password" name="userPassword" />
         <button type="submit">Login</button>
       </form>
+      <button
+        className="login__showPassword"
+        onClick={() => {
+          setshouldShowPasswordReset(true);
+        }}
+      >
+        Forgot Password? Get Password Reset Link
+      </button>
     </div>
   );
 }
